286fa5698bea3c01934c3ec5d19341aae54b08c2
Sifern <w.a.laarakkers@student.rug.nl>
2023-02-08 10:38:49 -0800

    Update list.lua

--- list.lua
+++ list.lua
@@ -9,23 +9,11 @@ local _, t = ...
 
 t.case_table = {
     [0] = {
-        -- These fields are always required. The case will be ignored by
-        -- Cutpurse without them.
-        name = "Thrall",  -- The last known name of the listed toon.
-        url = "https://discord.com/channels/some_discord/channel1",  -- A URL to the evidence against this player.
-
-        -- It is required to have ONE OF category or description, but 
-        -- including both is very helpful for users to tailor their alerts.
-        description = "Stole all the gold from a gdkp.",  -- A short verbal description of the case and offence.
-        category = "gdkp",  -- A category, should be one of "dungeon", "raid", "trade", "gdkp", "harassment"
-                            -- If the given category is none of these, it will be ignored.
-
-        -- These fields are optional.
-        guid = "Player-GDSAKG-53295G",  -- The GUID of the player. This is a more powerful identifier than the toon's name,
-                                        -- and persists through most types of reroll.
-        aliases = {"Warchief"},  -- a list of the previous aliases this player has gone under.
-        class = "WARRIOR",  -- The unit class, in allcaps english, no spaces, sometimes called the classFilename.
-                            -- See https://wowpedia.fandom.com/wiki/API_UnitClass for more info.
+        name = "Arthas",
+        category = "raid",
+        description = "Got a bit too upset at demons and had a strop.",
+        url = "discord url to proof",
+        class = "DEATHKNIGHT"
 
     },
 
